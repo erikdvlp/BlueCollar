21a0f09f9c5c290fa777b3e2c9110d6913fb6ffa
Erik Taylor <erikdvlp@gmail.com>
2020-12-22 18:37:55 -0500

    Update job params

--- api/src/main/java/com/bluecollar/jobmanagement/controller/JobManagementController.java
+++ api/src/main/java/com/bluecollar/jobmanagement/controller/JobManagementController.java
@@ -5,6 +5,8 @@ import com.bluecollar.jobmanagement.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,17 +16,36 @@ public class JobManagementController
 	@Autowired  
 	JobService jobService;
 	
-	//create job
 	@PostMapping("/createjob")
-	public String createJob(@RequestParam(value = "clientId") int clientId, @RequestParam(value = "jobCategoryId") int jobCategoryId, @RequestParam(value = "text") String text)
+	public void createJob(
+		@RequestParam(value = "clientId") int clientId,
+		@RequestParam(value = "jobCategoryId") int jobCategoryId,
+		@RequestParam(value = "text") String text)
 	{
 		jobService.saveOrUpdate(new Job(clientId, jobCategoryId, text));
-		return "Success";
 	}
 
-	//update job
+	@PatchMapping("/updatejob")
+	public void updateJob(
+		@RequestParam(value = "jobId") Integer jobId,
+		@RequestParam(value = "clientId", required = false) Integer clientId,
+		@RequestParam(value = "workerId", required = false) Integer workerId,
+		@RequestParam(value = "jobCategoryId", required = false) Integer jobCategoryId,
+		@RequestParam(value = "text", required = false) String text,
+		@RequestParam(value = "status", required = false) String status)
+	{
+		//implement
+	}
 
-	//delete job
+	@DeleteMapping("/deletejob")
+	public void deleteJob()
+	{
+		//implement
+	}
 
-	//search jobs
+	@GetMapping("/getjobs")
+	public void getJobs()
+	{
+		//implement
+	}
 }
